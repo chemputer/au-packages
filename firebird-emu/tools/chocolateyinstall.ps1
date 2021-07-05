94aa0d73593372b3ab1321df06db4240a8b3655f
Chemputer <46601333+chemputer@users.noreply.github.com>
2021-07-05 16:33:14 -0400

    Added firebird-emu chocolatey package.

---
+++ firebird-emu/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/nspire-emus/firebird/releases/download/v1.5/firebird-windows.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'firebird-emu*'
+
+  checksum      = 'DA127B5470B9A1B9DC8B0FBC9AF62FD34A7B4D884F6BB1503E93C41C5A9E7701'
+  checksumType  = 'sha256'
+
+}
+
+Install-ChocolateyZipPackage @packageArgs
